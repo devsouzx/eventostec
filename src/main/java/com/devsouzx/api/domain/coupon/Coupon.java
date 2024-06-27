599fdb4cf875eae6595b9228185d2d29ee0af456
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-27 18:54:22 -0300

    feat: creating entities

---
+++ src/main/java/com/devsouzx/api/domain/coupon/Coupon.java
@@ -0,0 +1,31 @@
+package com.devsouzx.api.domain.coupon;
+
+import com.devsouzx.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "coupon")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date validate;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
