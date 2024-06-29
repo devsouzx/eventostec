bc39f4555b74424700e1cdfa32242d2553e43d7d
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-29 16:47:01 -0300

    feat: add endpoint to search events

--- src/main/java/com/devsouzx/api/domain/event/EventResponseDTO.java
+++ src/main/java/com/devsouzx/api/domain/event/EventResponseDTO.java
@@ -3,5 +3,5 @@ package com.devsouzx.api.domain.event;
 import java.util.Date;
 import java.util.UUID;
 
-public record EventResponseDTO(UUID id, String title, String description, Date date, String city, String state, Boolean remote, String eventUrl, String image) {
+public record EventResponseDTO(UUID id, String title, String description, Date date, String city, String state, Boolean remote, String eventUrl, String imgUrl) {
 }
--- src/main/java/com/devsouzx/api/services/EventService.java
+++ src/main/java/com/devsouzx/api/services/EventService.java
@@ -6,7 +6,6 @@ import com.devsouzx.api.domain.event.Event;
 import com.devsouzx.api.domain.event.EventDetailsDTO;
 import com.devsouzx.api.domain.event.EventRequestDTO;
 import com.devsouzx.api.domain.event.EventResponseDTO;
-import com.devsouzx.api.repositories.AddressRepository;
 import com.devsouzx.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,18 +33,18 @@ public class EventService {
     private AmazonS3 s3Client;
 
     @Autowired
-    private EventRepository eventRepository;
-
-    @Autowired
     private AddressService addressService;
 
     @Autowired
     private CouponService couponService;
 
-    public Event createEvent(EventRequestDTO data) {
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Event createEvent(EventRequestDTO data){
         String imgUrl = null;
 
-        if (data.image() != null) {
+        if(data.image() != null){
             imgUrl = this.uploadImg(data.image());
         }
 
@@ -69,30 +68,6 @@ public class EventService {
     public List<EventResponseDTO> getUpcomingEvents(int page, int size){
         Pageable pageable = PageRequest.of(page, size);
         Page<Event> eventsPage = this.eventRepository.findUpcomingEvents(new Date(), pageable);
-        return eventsPage.map(event -> new EventResponseDTO(
-                        event.getId(),
-                        event.getTitle(),
-                        event.getDescription(),
-                        event.getDate(),
-                        event.getAddress() != null ? event.getAddress().getCity() : "",
-                        event.getAddress() != null ? event.getAddress().getUf() : "",
-                        event.getRemote(),
-                        event.getEventUrl(),
-                        event.getImgUrl())
-                )
-                .stream().toList();
-    }
-
-    public List<EventResponseDTO> getFilteredEvents(int page, int size, String title, String city, String uf, Date startDate, Date endDate){
-        title = (title != null) ? title : "";
-        city = (city != null) ? city : "";
-        uf = (uf != null) ? uf : "";
-        startDate = (startDate != null) ? startDate : new Date(0);
-        endDate = (endDate != null) ? endDate : new Date();
-
-        Pageable pageable = PageRequest.of(page, size);
-
-        Page<Event> eventsPage = this.eventRepository.findFilteredEvents(title, city, uf, startDate, endDate, pageable);
         return eventsPage.map(event -> new EventResponseDTO(
                         event.getId(),
                         event.getTitle(),
@@ -132,16 +107,58 @@ public class EventService {
                 couponDTOs);
     }
 
-    private String uploadImg(MultipartFile multipartFile) {
-        String fileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+    public List<EventResponseDTO> searchEvents(String title){
+        title = (title != null) ? title : "";
+
+        List<Event> eventsList = this.eventRepository.findEventsByTitle(title);
+        return eventsList.stream().map(event -> new EventResponseDTO(
+                        event.getId(),
+                        event.getTitle(),
+                        event.getDescription(),
+                        event.getDate(),
+                        event.getAddress() != null ? event.getAddress().getCity() : "",
+                        event.getAddress() != null ? event.getAddress().getUf() : "",
+                        event.getRemote(),
+                        event.getEventUrl(),
+                        event.getImgUrl())
+                )
+                .toList();
+    }
+
+    public List<EventResponseDTO> getFilteredEvents(int page, int size, String city, String uf, Date startDate, Date endDate){
+        city = (city != null) ? city : "";
+        uf = (uf != null) ? uf : "";
+        startDate = (startDate != null) ? startDate : new Date(0);
+        endDate = (endDate != null) ? endDate : new Date();
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Event> eventsPage = this.eventRepository.findFilteredEvents(city, uf, startDate, endDate, pageable);
+        return eventsPage.map(event -> new EventResponseDTO(
+                        event.getId(),
+                        event.getTitle(),
+                        event.getDescription(),
+                        event.getDate(),
+                        event.getAddress() != null ? event.getAddress().getCity() : "",
+                        event.getAddress() != null ? event.getAddress().getUf() : "",
+                        event.getRemote(),
+                        event.getEventUrl(),
+                        event.getImgUrl())
+                )
+                .stream().toList();
+    }
+
+    private String uploadImg(MultipartFile multipartFile){
+        String filename = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
 
         try {
             File file = this.convertMultipartToFile(multipartFile);
-            s3Client.putObject(bucketName, fileName, file);
+            s3Client.putObject(bucketName, filename, file);
             file.delete();
-            return s3Client.getUrl(bucketName, fileName).toString();
+            return s3Client.getUrl(bucketName, filename).toString();
         } catch (Exception e) {
-            System.out.println("Erro ao subir arquivo");
+            System.out.println("erro ao subir arquivo");
+            System.out.println(e.getMessage());
             return "";
         }
     }
